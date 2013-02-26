c1bf7fa0b6d6ced3cb15d64f2fe86baa57983988
Michael Kessler <mkessler31@gmail.com>
2013-02-26 07:46:17 -0600

    Finished demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MikesApp::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
