35bfabaee934e7f63dc0babf5155a436d2eaf9b7
Michael Kessler <mkessler31@gmail.com>
2013-02-26 07:24:54 -0600

    Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MikesApp::Application.config.secret_token = 'e79429b5a83646b504e8c1206b75fabcfa26c60ebf4df53688f8a9d8f81649eac43de178d5d070a3df182e95a64fd4feb1aafdc2f5edb7b8ed587a698fdb776a'
